149d3993fec72f8cf6c609af74ec0f82cc050721
Yago Oliveira Venci <yago_venci@yahoo.com.br>
2021-12-30 16:57:34 -0300

    Criando jogos com python, aulas alura

---
+++ jogo_adivinha.py
@@ -0,0 +1,49 @@
+import random
+
+
+def jogar():
+    '''
+    BEM VINDO AO JOGO DE ADIVINHAÇÃO!
+
+    '''
+
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
+
+    print ("Qual nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
+
+    nivel = int(input("Define o nivél: "))
+
+    if(nivel == 1):
+        total_de_tentativas = 20
+    elif(nivel == 2):
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+    rodada = 1
+
+    for rodada in range (1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada,total_de_tentativas))
+        chute_str = input("Digite um número entre 1 e 100:")
+        print("Você digitou", chute_str)
+        chute = int(chute_str)
+
+        acertou = chute == numero_secreto
+        maior = chute > numero_secreto
+        menor = chute < numero_secreto
+
+        if(acertou):
+            print("Você acertou e fez {} pontos".format(pontos))
+            break
+        else:
+            if(maior):
+                print("Você errou! O seu chute foi maior do que o número secreto.")
+            elif(menor):
+                print("Você errou! O seu chute foi menor do que o número secreto.")
+            pontos_perdidos = abs (numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+
+
